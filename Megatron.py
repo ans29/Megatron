504fdf9efe4a5380b5924f45538e782a7713add6
Anshul <https://AnshPrime@bitbucket.org>
2018-01-19 12:53:30 +0530

    parsing 1.1

--- Megatron.py
+++ Megatron.py
@@ -11,13 +11,43 @@ readM.read_metadata(table_names)
 #=========PARSING CMD================================
 cmd_raw = sys.argv[1:]
 print ("cmd", end = " : ");
-#re.split('; |, |\*|\n',a)
-cmd_split = re.split( '; |,| |\n' , (cmd_raw[0].lower()))
-print(cmd_split)
 cmd_split = ((cmd_raw[0].lower()).replace(",", " ")).split()
+
+if (cmd_split[-1][-1] != ';'):
+	print ("\tSYNTAX ERR : missing ';'")
+	exit()
+
 print(cmd_split)
-print (cmd_split[-1])
-print (cmd_split[-1][-1])
+
+select_list = []
+from_list = []
+condition_list = []
+flag = 0
+for wrd in cmd_split:
+	if (wrd == "select"):
+		flag = 1
+	elif (wrd == "from"):
+		flag = 2
+	elif (wrd == "where"):
+		flag = 3
+	elif (flag == 1):
+		select_list.append (wrd)
+	elif (flag == 2):
+		from_list.append (wrd)
+	elif (flag == 3):
+		condition_list.append (wrd)
+
+print ("\nSELECT" , end = " : ")
+for wrd in select_list:
+	print (wrd, end = " ")
+print ("\nFROM", end = " : ")
+for wrd in from_list:
+	print (wrd, end = " ")
+print ("\nWHERE" , end = " : ")
+for wrd in condition_list:
+	print (wrd, end = " ")
+
+
 #=====================================================
 '''
 	select  
