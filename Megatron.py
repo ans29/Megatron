29849b15b95591d813da71308f70f65a94f3202f
Anshul <https://AnshPrime@bitbucket.org>
2018-01-19 12:13:15 +0530

    parsing 1

--- Megatron.py
+++ Megatron.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from collections import defaultdict #for multimap
+import re  							#for spliting over multiple delimeters
 import ReadMetadata as readM
 
 table_names = defaultdict(list)  #MULTIMAP
@@ -10,8 +11,20 @@ readM.read_metadata(table_names)
 #=========PARSING CMD================================
 cmd_raw = sys.argv[1:]
 print ("cmd", end = " : ");
-cmd_split = (cmd_raw[0].lower()).split()
+#re.split('; |, |\*|\n',a)
+cmd_split = re.split( '; |,| |\n' , (cmd_raw[0].lower()))
 print(cmd_split)
-
+cmd_split = ((cmd_raw[0].lower()).replace(",", " ")).split()
+print(cmd_split)
+print (cmd_split[-1])
+print (cmd_split[-1][-1])
 #=====================================================
+'''
+	select  
+			(*, A, A,B,C,) (sum(A), avg(A)) + distinct|all ) 
+	from
+			tableName
+	where
+			asdasd
+'''
 
