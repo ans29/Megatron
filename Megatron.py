9d324df72ee0ff38430911e3dafa28f75e92a480
Anshul <https://AnshPrime@bitbucket.org>
2018-01-22 23:05:43 +0530

    data structure

--- Megatron.py
+++ Megatron.py
@@ -35,13 +35,17 @@ for table in table_names:
 	with open(table_name, 'r') as f:	#Reading full CSV
 		reader = csv.reader (f)
 		listx = list (reader)			
-	#INSERT IN LIST
-	for sublist in listx:
-		for item,col in zip(sublist, table_names[table]):
-			key = table+"."+col
-			main_db_list [key].append(item)
-	#INSERT IN MAP
 	
+	count = -1
+	for sublist in listx:
+		count += 1
+		for item, col in zip(sublist, table_names[table]):
+			key = table+"."+col
+		#INSERT IN LIST
+			main_db_list [key].append(item)
+		#INSERT IN MAP
+		#	main_db_multimap [key][item] = count
+
 
 for d in main_db_list:
 	print (d+ ": "+ str(main_db_list[d]))
