318f8b18585b5d6bc75fe579f8eacf13f80ee0fb
Anshul <https://AnshPrime@bitbucket.org>
2018-01-22 22:42:09 +0530

    data structure

--- Megatron.py
+++ Megatron.py
@@ -3,14 +3,47 @@ import os
 from collections import defaultdict #for multimap
 import ReadMetadata as readM
 import ParserFile as parser
+import csv 			# to read csv files
 
+
+#======== METADATA ===================================
 table_names = defaultdict(list)  #MULTIMAP
 readM.read_metadata(table_names)
 
-
-#=========PARSING CMD================================
+#======== PARSING CMD ================================
 cmd_raw = sys.argv[1:]
 select_list = []
 from_list = []
 condition_list = []
 parser.parse (cmd_raw, select_list, from_list, condition_list)
+
+#======== DATASTRUCTURE ==============================
+#1. LIST of lists (T1.A[], T2.X[], T2.Y[])
+main_db_list = defaultdict (list) #MULTIMAP
+
+#2. LIST of multimap,
+# tablename.col name gives which hashTable, 
+# then second key gives key for hash
+# output is a list of values in list form
+main_db_multimap = [defaultdict(list)]
+
+#======== READING DB =================================
+print ("\n")
+
+for table in table_names:
+	table_name = table + ".csv"
+	with open(table_name, 'r') as f:	#Reading full CSV
+		reader = csv.reader (f)
+		listx = list (reader)			
+	#INSERT IN LIST
+	for sublist in listx:
+		for item,col in zip(sublist, table_names[table]):
+			key = table+"."+col
+			main_db_list [key].append(item)
+	#INSERT IN MAP
+	
+
+for d in main_db_list:
+	print (d+ ": "+ str(main_db_list[d]))
+
+
